2b4255c490e9a8abae93f8c30c3342849c70b9a3
Hany AbuShall <32711875+hanyabushall@users.noreply.github.com>
2018-11-15 14:42:31 -0500

    Copy the test files I have created on my machine before using github to the Git Repository.

---
+++ Testing/Test01.py
@@ -0,0 +1,9 @@
+#Hello, this is my first Python test script.
+
+Var1 = float(input("Enter first number: "))
+Var2 = float(input("Enter second number: "))
+
+print("Adding these two numbers gives us: ", Var1 + Var2)
+
+for i in range(1, 3):
+    print("Hello")
