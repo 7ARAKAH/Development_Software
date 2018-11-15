3f0b5414bab205f370f09a491f99e5e2fb015ee2
Hany AbuShall <32711875+hanyabushall@users.noreply.github.com>
2018-11-15 14:45:25 -0500

    I added Var3 to Test01.py.

--- Testing/Test01.py
+++ Testing/Test01.py
@@ -2,8 +2,9 @@
 
 Var1 = float(input("Enter first number: "))
 Var2 = float(input("Enter second number: "))
+Var3 = float(input("Enter third number: "))
 
-print("Adding these two numbers gives us: ", Var1 + Var2)
+print("Adding these two numbers gives us: ", Var1 + Var2 + Var3)
 
 for i in range(1, 3):
     print("Hello")
